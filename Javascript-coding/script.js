1f4fe54f879def5043fc25b59f18c5087edcea8d
chandrabose165 <varisibunny@gmail.com>
2026-07-23 19:00:42 +0530

    variables and Datatypes

---
+++ Javascript-coding/script.js
@@ -0,0 +1,6 @@
+// First type of run the code in the vscode using extension of .js so it can treated as script file
+let Name = "Chandrabose";
+const RollNo = "2211CS030165";
+console.log(Name);
+console.log(RollNo);
+alert("Welcome to javascript");
